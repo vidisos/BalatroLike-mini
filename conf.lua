3e999aa6915a9e97ee2869bf09087df69ade63a2
vidisos-portable <vid.iskra11@gmail.com>
2026-03-29 22:28:34 +0200

    final commit before .exe file things

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.modules.joystick = false
     --TODO remember to turn console to false in final version or smth
-    t.console = true
+    t.console = false
     t.window.title = "Poinker"
     t.window.icon = "src/images/icon.png"
 
