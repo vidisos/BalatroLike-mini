27c52ff49cce8effcce77fc6fc163c022d8f098d
vidisos-portable <vid.iskra11@gmail.com>
2026-04-10 19:51:18 +0200

    one comment change lol

--- conf.lua
+++ conf.lua
@@ -1,7 +1,6 @@
 function love.conf(t)
     t.modules.joystick = false
     --TODO remember to turn console to false in final version
-    --TODO TRY TO IMPLEMENT TUTORIAL? (JUST LIKE ONE SCREEN OF TEXT)
     t.console = false
     t.window.title = "Poinker"
     t.window.icon = "src/images/icon.png"
